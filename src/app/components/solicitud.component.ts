a2d4c20eb5c57fb43ecf424f6dd9f9452c6797b1
Rodrigo A <linelsk@hotmail.com>
2019-06-07 14:22:26 -0500

    modal informes
    Modal con segimiento reporte

--- src/app/components/solicitud.component.ts
+++ src/app/components/solicitud.component.ts
@@ -1,196 +1,197 @@
-import { Component} from '@angular/core';
-import { DatosService } from '../services/datos.service';
+import { Component, Inject } from '@angular/core';
 import { Clearing } from '../models/clearing';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import * as jquery from 'jquery';
-
+import { DatosService } from '../services/datos.service';
 
 @Component({
   selector: 'solicitud',
   templateUrl: '../views/solicitud.html',
   providers: [DatosService]
 })
+
 export class SolicitudComponent {
-    public mask_telefono = ['+',/[1-9]/, /\d/,' ','(', /\d/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-    public mask_cp = [/\d/, /\d/, /\d/, /\d/, /\d/];
-    public mask_comienzo = [/\d/, /\d/, /\d/, /\d/];
-    public titulo: string;
-    public clearing = new Clearing();
-    public respuesta =  Clearing;
-    public pais;
-    public mensaje;
-    public paises: any = [];
-    public ciudades: any = [];
-    public localidades: any = [];
-    public materiasenfoque: any = [];
-    public comunidades: any = [];
-    public materiaslegales: any = [];
-    public conocimientosprobono: any = [];
-    public edicion: boolean;
-    public filesToUpload;
-    public resultUpload;
-    public historia:boolean = true;
-  
+  public mask_telefono = ['+', /[1-9]/, /\d/, ' ', '(', /\d/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+  public mask_cp = [/\d/, /\d/, /\d/, /\d/, /\d/];
+  public mask_comienzo = [/\d/, /\d/, /\d/, /\d/];
+  public titulo: string;
+  public clearing = new Clearing();
+  public respuesta = Clearing;
+  public pais;
+  public mensaje;
+  public paises: any = [];
+  public ciudades: any = [];
+  public localidades: any = [];
+  public materiasenfoque: any = [];
+  public comunidades: any = [];
+  public materiaslegales: any = [];
+  public conocimientosprobono: any = [];
+  public edicion: boolean;
+  public filesToUpload;
+  public resultUpload;
+  public historia: boolean = true;
+
   constructor(
-      private _datosService: DatosService,
-      private _route : ActivatedRoute,
-      private _router : Router,
-      public snackBar: MatSnackBar,
-    ) { 
-        this.titulo = "NUEVA SOLICITUD";
+    private _datosService: DatosService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    public snackBar: MatSnackBar,
+  ) {
+    this.titulo = "NUEVA SOLICITUD";
+  }
+
+  ngOnInit() {
+    this.edicion = true;
+    this.clearing.acuerdo_entrega_docs = "0";
+    this.clearing.terminos_condiciones = "0";
+    this.clearing.cuenta_consejo_independiente = "0";
+    this.clearing.recibio_ayuda_probono = "0";
+    this.clearing.otra_red_abogados = "0";
+    this.clearing.cuenta_con_apoyo = "0";
+    this.clearing.fin_religioso = "0";
+    this.clearing.miembro_red_probono = "0";
+    this.clearing.tiene_discriminacion = "0";
+    this.getPaises();
+    this.getCiudades();
+    this.getLocalidades();
+    this.getMateriasEnfoque();
+    this.getCominidades();
+    this.getMateriasLegales();
+    this.getConocimientoProbono();
+  }
+  //Catalogos
+  getPaises() {
+    this._datosService.getPaises().subscribe(
+      result => {
+        this.paises = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+  selectEstado(): void {
+    console.log(this.clearing.pais_osc);
+  }
+
+  getCiudades() {
+    this._datosService.getCiudades().subscribe(
+      result => {
+        this.ciudades = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  getLocalidades() {
+    this._datosService.getLocalidades().subscribe(
+      result => {
+        this.localidades = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  getMateriasEnfoque() {
+    this._datosService.getMateriasEnfoque().subscribe(
+      result => {
+        this.materiasenfoque = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  getCominidades() {
+    this._datosService.getComunidades().subscribe(
+      result => {
+        this.comunidades = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  getMateriasLegales() {
+    this._datosService.getMateriasLegales().subscribe(
+      result => {
+        this.materiaslegales = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  getConocimientoProbono() {
+    this._datosService.getConocimientoProbono().subscribe(
+      result => {
+        this.conocimientosprobono = result;
+        // console.log(result);
+      },
+      error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+      }
+    );
+  }
+
+  //Recoge el fichero y lo pone en un array
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files; //Recoge nuestro fichero y lo pone en un array
+    console.log(this.filesToUpload);
+  }
+  //Formulario solicitud 
+  enviarSolicitud() {
+    this.clearing.estado_clearing = 1;
+    this.clearing.subestado_clearing = null;
+    this.clearing.entidad_probono = null;
+    // console.log(this.clearing);
+    this._datosService.clearing(this.clearing)
+      .subscribe((value) => {
+        // console.log(value);
+        this.clearing = value.clearing;
+        console.log(this.clearing);
+        // console.log("Aqui esta el id"+this.clearing.id);
+        this.mensaje = 'Su solicitud fue enviada con éxito';
+        this.snackBar.open(this.mensaje, "", {
+          duration: 5000,
+          verticalPosition: 'bottom',
+          horizontalPosition: 'right'
+        });
+        // this._router.navigate(['/solicitudes']);
+      },
+        (error) => {
+          console.log(error);
+        });
+  }
+  //Validacion del formulario
+  validaciones(event) {
+    for (var i = 0; i < event.length; i++) {
+      if (!event[i].valid) {
+        $('#' + event[i].name).focus();
+        break;
+      }
     }
-
-      ngOnInit() {
-        this.edicion =  true;
-        this.clearing.acuerdo_entrega_docs = "0";
-        this.clearing.terminos_condiciones = "0";
-        this.clearing.cuenta_consejo_independiente = "0";
-        this.clearing.recibio_ayuda_probono = "0";
-        this.clearing.otra_red_abogados = "0";
-        this.clearing.cuenta_con_apoyo = "0";
-        this.clearing.fin_religioso = "0";
-        this.clearing.miembro_red_probono = "0";
-        this.clearing.tiene_discriminacion = "0";
-        this.getPaises();
-        this.getCiudades();
-        this.getLocalidades();
-        this.getMateriasEnfoque();
-        this.getCominidades();
-        this.getMateriasLegales();
-        this.getConocimientoProbono();
-      }
-      //Catalogos
-      getPaises() {
-        this._datosService.getPaises().subscribe( 
-          result => {
-                this.paises = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-      selectEstado(): void {
-        console.log(this.clearing.pais_osc);
-      }
-      
-      getCiudades() {
-        this._datosService.getCiudades().subscribe( 
-          result => {
-                this.ciudades = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-
-      getLocalidades() {
-        this._datosService.getLocalidades().subscribe( 
-          result => {
-                this.localidades = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-
-      getMateriasEnfoque() {
-        this._datosService.getMateriasEnfoque().subscribe( 
-          result => {
-                this.materiasenfoque = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-
-      getCominidades() {
-        this._datosService.getComunidades().subscribe( 
-          result => {
-                this.comunidades = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-
-      getMateriasLegales() {
-        this._datosService.getMateriasLegales().subscribe( 
-          result => {
-                this.materiaslegales = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-
-      getConocimientoProbono() {
-        this._datosService.getConocimientoProbono().subscribe( 
-          result => {
-                this.conocimientosprobono = result;
-                // console.log(result);
-            },
-            error => {
-            var errorMessage = <any>error;
-            console.log(errorMessage);
-            }
-        );
-      }
-  
-      //Recoge el fichero y lo pone en un array
-      fileChangeEvent(fileInput: any) {
-        this.filesToUpload = <Array<File>>fileInput.target.files; //Recoge nuestro fichero y lo pone en un array
-        console.log(this.filesToUpload);
-      }
-      //Formulario solicitud 
-      enviarSolicitud() {
-          this.clearing.estado_clearing = 1;
-          this.clearing.subestado_clearing = null;
-          this.clearing.entidad_probono = null;
-          // console.log(this.clearing);
-          this._datosService.clearing(this.clearing)
-              .subscribe((value) => {
-                // console.log(value);
-                this.clearing = value.clearing;
-                console.log(this.clearing);
-                // console.log("Aqui esta el id"+this.clearing.id);
-                this.mensaje = 'Su solicitud fue enviada con éxito';
-                this.snackBar.open(this.mensaje, "", {
-                  duration: 5000,
-                  verticalPosition: 'bottom',
-                  horizontalPosition: 'right'
-                  });
-              // this._router.navigate(['/solicitudes']);
-          }, 
-          (error) => {
-            console.log(error);
-          });
-      }
-      //Validacion del formulario
-      validaciones(event) {
-        for(var i=0; i<event.length; i++) {
-          if(!event[i].valid) {
-            $('#'+ event[i].name).focus();
-            break;
-          }
-        }
-      }
+  }
 }
+
--- src/app/services/datos.service.ts
+++ src/app/services/datos.service.ts
@@ -5,169 +5,181 @@ import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from './globals';
 import { Clearing } from '../models/clearing';
-import {map} from 'rxjs/operators'; 
+import { map } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class DatosService {
-    // public url: string;
-    public url = "http://apiclearing.demo-minimalist.com/";
-    /* public url = "http://23.253.173.64:8081/api/"; */
+  // public url: string;
+  //public url = "http://apiclearing.demo-minimalist.com/";
+  public url = "http://23.253.173.64:8081/api/";
 
-     _headers = new HttpHeaders( {
-        "Content-Type": "application/json",
-        "Authorization": "Token "+ localStorage.getItem("token")
-     });
+  _headers = new HttpHeaders({
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + localStorage.getItem("token")
+  });
 
-     _contentHtml = new HttpHeaders({
-        "Content-Type": "application/x-www-form-urlencoded"
-     });
+  _contentHtml = new HttpHeaders({
+    "Content-Type": "application/x-www-form-urlencoded"
+  });
 
-    constructor(
-        private http: HttpClient,
-        private _http: Http,
-        private _authServices: AuthService,
-    ) {
-    }
+  constructor(
+    private http: HttpClient,
+    private _http: Http,
+    private _authServices: AuthService,
+  ) {
+  }
 
-    //Todo lo de usuarios
-    login(parametros): Observable<any> {     
-        return this.http.post(this.url + 'rest-auth/login/', parametros);
-    }
-    logout() {
-        return this.http.get(this.url+'rest-auth/logout/', {headers: this._headers} );
-    }
-    changePassword(parametros) {
-        // console.log(this._headers.get('Authorization'));
-        return this.http.post(this.url + 'rest-auth/password/change/', parametros, {headers: this._headers}); 
-    }
-    recuperarContrasenia(email) {
-        return this.http.post(this.url + 'rest-auth/password/reset/', email); 
-    } 
-    
-    registro(parametros) {     
-        return this.http.post(this.url + 'rest-auth/registration/', parametros);
-    }
-     //Sube un fichero de imagen por POST 
-     subirArchivos(params: Array<string>, files: Array<File>) {
-         console.log(files);
-        return new Promise((resolve, reject)=> {
-            //Simular un formulario normal
-            var formData: any = new FormData();
-            //Instacia de XMLHttpRequest deja disponible el objeto para las peticiones ajax
-            var xhr = new XMLHttpRequest();
+  service_general_post(url, parametros): Observable<any> {
+    return this.http.post(this.url + url, parametros, { headers: this._headers });
+  }
+
+  service_general_get(url): Observable<any> {
+    return this.http.get(this.url + url, { headers: this._headers });
+  }
+
+  service_general_params_get(url, parametros): Observable<any> {
+    return this.http.get(this.url + url, { headers: this._headers, params: parametros });
+  }
+
+  //Todo lo de usuarios
+  login(parametros): Observable<any> {
+    return this.http.post(this.url + 'rest-auth/login/', parametros);
+  }
+  logout() {
+    return this.http.get(this.url + 'rest-auth/logout/', { headers: this._headers });
+  }
+  changePassword(parametros) {
+    // console.log(this._headers.get('Authorization'));
+    return this.http.post(this.url + 'rest-auth/password/change/', parametros, { headers: this._headers });
+  }
+  recuperarContrasenia(email) {
+    return this.http.post(this.url + 'rest-auth/password/reset/', email);
+  }
+
+  registro(parametros) {
+    return this.http.post(this.url + 'rest-auth/registration/', parametros);
+  }
+  //Sube un fichero de imagen por POST 
+  subirArchivos(params: Array<string>, files: Array<File>) {
+    console.log(files);
+    return new Promise((resolve, reject) => {
+      //Simular un formulario normal
+      var formData: any = new FormData();
+      //Instacia de XMLHttpRequest deja disponible el objeto para las peticiones ajax
+      var xhr = new XMLHttpRequest();
 
 
-            for(var i=0; i < files.length; i++) {
-                formData.append('uploads[]', files[i], files[i].name);
-            }
+      for (var i = 0; i < files.length; i++) {
+        formData.append('uploads[]', files[i], files[i].name);
+      }
 
-            //Cuando la peticion de ajax este preparada
-            xhr.onreadystatechange = function() {
-                if(xhr.readyState == 4) {
-                    if(xhr.status == 200 ) {
-                        resolve(JSON.parse(xhr.response));
-                    } else {
-                        reject(xhr.response);
-                    }
-                }
-            };
-            xhr.open("POST", this.url+'archivos/', true);
-            xhr.send(formData);
-        });
-    }
+      //Cuando la peticion de ajax este preparada
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(xhr.response);
+          }
+        }
+      };
+      xhr.open("POST", this.url + 'archivos/', true);
+      xhr.send(formData);
+    });
+  }
 
-    //Proceso de clearing inicial
-    // clearing(parametros) {
-    //     return this.http.post(this.url + 'clearing/', parametros);
-    // }
-    clearing(clearing : Clearing) {
-        let json = JSON.stringify(clearing);
-        let parametros = json;
-        let headers = new Headers({'Content-Type':'application/json'});
-         return this._http.post(this.url + 'clearing/', parametros, {headers : headers})
-         .pipe(map(res => res.json()));
-    }
-    //Actualizar el formulario
-    actualizarFormulario(id, parametros) {
-        let headers = new HttpHeaders();
-        headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-        return this.http.put(this.url + 'clearing/'+id+'/', parametros, {headers: headers});
-    }
-    getSolicitudesLista1() {
-        /* return this.http.get(this.url+'clearing/'); */
-        return this.http.get('http://23.253.173.64:8081/api/clearing/Search/', this._authServices.authHttpOptions());
-        // var body = 'palabra=' + '&fecha_solicitud=' + '&estado_solicitud='+ '&sub_estado_solicitud='+ 
-    } 
+  //Proceso de clearing inicial
+  // clearing(parametros) {
+  //     return this.http.post(this.url + 'clearing/', parametros);
+  // }
+  clearing(clearing: Clearing) {
+    let json = JSON.stringify(clearing);
+    let parametros = json;
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    return this._http.post(this.url + 'clearing/', parametros, { headers: headers })
+      .pipe(map(res => res.json()));
+  }
+  //Actualizar el formulario
+  actualizarFormulario(id, parametros) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    return this.http.put(this.url + 'clearing/' + id + '/', parametros, { headers: headers });
+  }
+  getSolicitudesLista1() {
+    /* return this.http.get(this.url+'clearing/'); */
+    return this.http.get('http://23.253.173.64:8081/api/clearing/Search/', this._authServices.authHttpOptions());
+    // var body = 'palabra=' + '&fecha_solicitud=' + '&estado_solicitud='+ '&sub_estado_solicitud='+ 
+  }
 
-    getSolicitudesLista(parametros){
-        //var body = 'palabra=' + palabra + '&fecha_solicitud=' + fechaSolicitud + '&estado_solicitud=' + estadoSolicitud + '&sub_estado_solicitud=' + subEstadoSolicitud;
-        return this.http.get('http://23.253.173.64:8081/api/clearing/Search/'+ parametros +'/', this._authServices.authHttpOptions());
-    }
+  getSolicitudesLista(parametros) {
+    //var body = 'palabra=' + palabra + '&fecha_solicitud=' + fechaSolicitud + '&estado_solicitud=' + estadoSolicitud + '&sub_estado_solicitud=' + subEstadoSolicitud;
+    return this.http.get('http://23.253.173.64:8081/api/clearing/Search/' + parametros + '/', this._authServices.authHttpOptions());
+  }
 
-    getSolicitud(id) {
-        return this.http.get(this.url+'clearing/'+id+'/');
-    }
+  getSolicitud(id) {
+    return this.http.get(this.url + 'clearing/' + id + '/');
+  }
 
-    //Catalogos
-    getPaises() {
-        return this.http.get(this.url + 'catalogos/country/', this._authServices.authHttpOptions());
-    }
-    getCiudades() {
-        return this.http.get(this.url + 'catalogs/city?id_country=1&id_state=1', this._authServices.authHttpOptions());
-    }
-    getLocalidades() {
-        return this.http.get(this.url + 'catalogos/localidades/', this._authServices.authHttpOptions());
-    }
-    getMateriasEnfoque() {
-        return this.http.get(this.url + 'catalogos/materias-enfoque/', this._authServices.authHttpOptions());
-    }
-    getComunidades() {
-        return this.http.get(this.url + 'catalogos/comunidades/', this._authServices.authHttpOptions());
-    }
-    getMateriasLegales() {
-        return this.http.get(this.url + 'catalogos/materias-legales/', this._authServices.authHttpOptions());
-    }
-    getConocimientoProbono() {
-        return this.http.get(this.url + 'catalogos/conocimiento-probono/', this._authServices.authHttpOptions());
-    }
-    getPeriodos(){
-        return this.http.get('http://23.253.173.64:8081/api/catalogs/Time_Frame', this._authServices.authHttpOptions());
-    }
-    getSubestados1(id:number) {
-        return this.http.get('http://23.253.173.64:8081/api/catalogs/Clearing_Sub_Status/'+ id + '/', this._authServices.authHttpOptions());
-    }
-    getEstadosSolicitud(){
-        /* return this.http.get(this.url + 'catalogs/Clearing_Status', this._authServices.authHttpOptions()); */
-        return this.http.get('http://23.253.173.64:8081/api/catalogs/Clearing_Status', this._authServices.authHttpOptions());
-    }
-    getSubestados(url, parametros) {
-        let headers = new HttpHeaders();
-        headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-        headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
+  //Catalogos
+  getPaises() {
+    return this.http.get(this.url + 'catalogos/country/', this._authServices.authHttpOptions());
+  }
+  getCiudades() {
+    return this.http.get(this.url + 'catalogs/city?id_country=1&id_state=1', this._authServices.authHttpOptions());
+  }
+  getLocalidades() {
+    return this.http.get(this.url + 'catalogos/localidades/', this._authServices.authHttpOptions());
+  }
+  getMateriasEnfoque() {
+    return this.http.get(this.url + 'catalogos/materias-enfoque/', this._authServices.authHttpOptions());
+  }
+  getComunidades() {
+    return this.http.get(this.url + 'catalogos/comunidades/', this._authServices.authHttpOptions());
+  }
+  getMateriasLegales() {
+    return this.http.get(this.url + 'catalogos/materias-legales/', this._authServices.authHttpOptions());
+  }
+  getConocimientoProbono() {
+    return this.http.get(this.url + 'catalogos/conocimiento-probono/', this._authServices.authHttpOptions());
+  }
+  getPeriodos() {
+    return this.http.get('http://23.253.173.64:8081/api/catalogs/Time_Frame', this._authServices.authHttpOptions());
+  }
+  getSubestados1(id: number) {
+    return this.http.get('http://23.253.173.64:8081/api/catalogs/Clearing_Sub_Status/' + id + '/', this._authServices.authHttpOptions());
+  }
+  getEstadosSolicitud() {
+    /* return this.http.get(this.url + 'catalogs/Clearing_Status', this._authServices.authHttpOptions()); */
+    return this.http.get('http://23.253.173.64:8081/api/catalogs/Clearing_Status', this._authServices.authHttpOptions());
+  }
+  getSubestados(url, parametros) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+    headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem("token"));
 
-        return this.http.get('http://23.253.173.64:8081/api/' + url, { headers: headers, params: parametros });
-        //return this.http.get(this.url + 'catalogos/subestados/Clearing_Sub_Status', this._authServices.authHttpOptionsParams(id));
-    }
+    return this.http.get('http://23.253.173.64:8081/api/' + url, { headers: headers, params: parametros });
+    //return this.http.get(this.url + 'catalogos/subestados/Clearing_Sub_Status', this._authServices.authHttpOptionsParams(id));
+  }
 
-    getFiltro(parametros){
-        return this.http.get('http://23.253.173.64:8081/api/clearing/Search/', this._authServices.authHttpOptionsParams(parametros));
-    }
+  getFiltro(parametros) {
+    return this.http.get('http://23.253.173.64:8081/api/clearing/Search/', this._authServices.authHttpOptionsParams(parametros));
+  }
 
-     getEstados() {
-        return this.http.get(this.url + 'catalogos/estados/', this._authServices.authHttpOptions());
-    }
-    getEntidadesProbono() {
-        return this.http.get(this.url + 'catalogos/entidades/', this._authServices.authHttpOptions());
-    }
-    //Apartado de estadisticas
-    getEstadisticas() {
-        return this.http.get('http://23.253.173.64:8081/api/clearing/Estadistica', this._authServices.authHttpOptions());
-    }
+  getEstados() {
+    return this.http.get(this.url + 'catalogos/estados/', this._authServices.authHttpOptions());
+  }
+  getEntidadesProbono() {
+    return this.http.get(this.url + 'catalogos/entidades/', this._authServices.authHttpOptions());
+  }
+  //Apartado de estadisticas
+  getEstadisticas() {
+    return this.http.get('http://23.253.173.64:8081/api/clearing/Estadistica', this._authServices.authHttpOptions());
+  }
 
-    getEstadisticasFecha(fecha_inicial, fecha_actual, periodo) {
-        var body = 'fecha_inicial='+fecha_inicial+'&fecha_actual='+fecha_actual + '&periodo=' + periodo;
-        return this.http.get('http://23.253.173.64:8081/api/clearing/Estadistica' + body + '/', this._authServices.authHttpOptions()); 
-    }
+  getEstadisticasFecha(fecha_inicial, fecha_actual, periodo) {
+    var body = 'fecha_inicial=' + fecha_inicial + '&fecha_actual=' + fecha_actual + '&periodo=' + periodo;
+    return this.http.get('http://23.253.173.64:8081/api/clearing/Estadistica' + body + '/', this._authServices.authHttpOptions());
+  }
 
 } //Final 
